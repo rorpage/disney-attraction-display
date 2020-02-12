c468dc0430f02b8234454fbcc937bab893f8ccb2
Robbie Page <robbie.page@e-gineering.com>
2020-02-12 12:17:13 -0500

    add wait time int to payload

--- api/info/[id].js
+++ api/info/[id].js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
 async function handler(req, resp) {
   var attraction_id = req.query.id || 80010191;
@@ -12,29 +12,33 @@ async function handler(req, resp) {
     id: attraction_info.id,
     name: attraction_info.name,
     parkId: parseInt(attraction_info.parkId),
+    wait: attraction_info.waitTime,
     waitTime: getAttractionWaitTime(attraction_info)
   };
 
   let utc_offset = getUtcOffset(attraction_info.parkId);
 
   let client_date = new Date();
-  let utc = client_date.getTime() + (client_date.getTimezoneOffset() * 60000);
-  let server_date = new Date(utc + (3600000 * utc_offset));
+  let utc = client_date.getTime() + client_date.getTimezoneOffset() * 60000;
+  let server_date = new Date(utc + 3600000 * utc_offset);
 
-  let hour = (server_date.getHours() + 11) % 12 + 1;
-  let hour_display = (hour < 10) ? `0${hour}` : hour;
+  let hour = ((server_date.getHours() + 11) % 12) + 1;
+  let hour_display = hour < 10 ? `0${hour}` : hour;
   let minutes = server_date.getMinutes();
-  let minutes_display = (minutes < 10) ? `0${minutes}` : minutes;
+  let minutes_display = minutes < 10 ? `0${minutes}` : minutes;
   let time = `${hour_display}:${minutes_display}`;
 
   let month = server_date.getMonth() + 1;
-  let month_display = (month < 10) ? `0${month}` : month;
+  let month_display = month < 10 ? `0${month}` : month;
   let day = server_date.getDate();
-  let day_display = (day < 10) ? `0${day}` : day;
+  let day_display = day < 10 ? `0${day}` : day;
   let date = `${month_display}/${day_display}`;
 
-  resp.setHeader("Access-Control-Allow-Origin", "*");
-  resp.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  resp.setHeader('Access-Control-Allow-Origin', '*');
+  resp.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   resp.json({
     attraction_info: attraction,
     temperature: Math.round(weather.currently.temperature),
@@ -44,7 +48,9 @@ async function handler(req, resp) {
 }
 
 async function getAttractionInformation(attraction_id) {
-  let attractionRequest = await fetch(`https://wdwnt-now-api.herokuapp.com/api/attractions/${attraction_id}`);
+  let attractionRequest = await fetch(
+    `https://wdwnt-now-api.herokuapp.com/api/attractions/${attraction_id}`
+  );
   return await attractionRequest.json();
 }
 
@@ -56,10 +62,10 @@ async function getWeatherInformation() {
 function getAttractionWaitTime(attraction_info) {
   let wait_time = attraction_info.currentStatus;
 
-  if (wait_time.includes("Closed")) {
-    return "Closed";
-  } else if (wait_time.includes("Temporary")) {
-    return "Temp. closure";
+  if (wait_time.includes('Closed')) {
+    return 'Closed';
+  } else if (wait_time.includes('Temporary')) {
+    return 'Temp. closure';
   }
 
   return attraction_info.currentStatus.replace('Posted wait', 'Wait');
@@ -71,16 +77,16 @@ function getUtcOffset(park_id) {
   // Anaheim
   if (park_id === 330339 || park_id === 336894) {
     utc_offset = -7;
-  // Tokyo
+    // Tokyo
   } else if (park_id === 1 || park_id === 2) {
     utc_offset = 9;
-  // Paris
+    // Paris
   } else if (park_id === 3 || park_id === 4) {
     utc_offset = 2;
-  // Hong Kong
+    // Hong Kong
   } else if (park_id === 5) {
     utc_offset = 8;
-  // Shanghai
+    // Shanghai
   } else if (park_id === 6) {
     utc_offset = 8;
   }
